c106912eb11f15ebf3fc4b8c0fb4256a6e436fa8
Mirco Kroon <M.Kroon-1@student.tudelft.nl>
2019-05-09 08:27:51 +0200

    fixed issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class Main {
 
+    int mode = -1;
+
     public static void main(String[] args) {
         String host = "localhost";
         int portRemote = 25565;
         int portLocal = 25570;
 
         ProxyServer proxy = new ProxyServer(portRemote, portLocal, host);
-        proxy.runServer((b) -> System.out.println(Arrays.toString(b)), (b) -> {});
+        proxy.runServer(new PacketHandler("server"), new PacketHandler("client"));
     }
 }
