34a413298e6f8615b1429e0b1635952172e1c5c2
Mirco Kroon <23699979+mircokroon@users.noreply.github.com>
2020-07-11 16:24:51 +0200

    Added manual authentication

--- src/main/java/proxy/auth/ClientAuthenticator.java
+++ src/main/java/proxy/auth/ClientAuthenticator.java
@@ -6,33 +6,119 @@ import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ClientAuthenticator {
     private static int STATUS_SUCCESS = 204;
+    private static String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/";
     private static String AUTH_URL = "https://sessionserver.mojang.com/session/minecraft/join";
 
     private LauncherProfiles profiles;
+    private AuthDetails manualDetails;
 
     /**
      * Initialise the authenticator class by reading from the JSON file.
      */
     public ClientAuthenticator() {
+
+        try {
+            Gson g = new Gson();
+            profiles = g.fromJson(getFile(), LauncherProfiles.class);
+        } catch (IOException ex) {
+            startAuthDialogue();
+        }
+
+    }
+
+    /**
+     * Ask the user for their username and access token, not as nice as getting it from the file but it's better than
+     * not working at all.
+     */
+    private void startAuthDialogue() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Cannot read or find launcher_profiles.json!\nYou can use launch option \"-m /path/to/.minecraft/\" to indicate the location of your Minecraft installation.");
+        System.out.println("Using manual authentication...");
+
+        String uuid = promptUuid(reader);
+        String token = promptAccessToken(reader);
+        this.manualDetails = new AuthDetails(uuid, token);
+    }
+
+    private static String retry(Prompt r) {
+        while (true) {
+            try {
+                return r.run();
+            } catch (IOException ex) { }
+        }
+    }
+
+    /**
+     * Ask the user for their access token, retry if they don't input one that looks correct.
+     */
+    private String promptAccessToken(BufferedReader reader) {
+        System.out.println();
+        System.out.println("Your access token is needed for authentication. It...");
+        System.out.println("\t- Can be found in launcher_profiles.json, inside your .minecraft directory (for the default launcher)");
+        System.out.println("\t- Should be named 'accessToken'");
+        System.out.println("\t- Is quite long (over 300 characters)");
+        System.out.println("\t- Can change when starting the game, so launch Minecraft before entering it");
+        System.out.println("If you have multiple accounts, make sure to pick the one matching the given username.");
+
+        return retry(() -> {
+            System.out.print("Access token: ");
+
+            // remove trailing spaces and quotes, some people will probably copy those by accident
+            String token = reader.readLine().trim().replaceAll("^\"|\"$", "");
+
+            if (token.length() != 308) {
+                System.out.println("The given access token is too " + (token.length() > 308 ? "long" : "short") + "!" +
+                                       " Make sure you are using the correct token.");
+                throw new IOException("Token too short");
+            }
+
+            return token;
+        });
+    }
+
+    /**
+     * Ask the user for their username and use it to retrieve the UUID. Retry if the username cannot be found.
+     */
+    private String promptUuid(BufferedReader reader) {
         Gson g = new Gson();
-        profiles = g.fromJson(getFile(), LauncherProfiles.class);
+
+        return retry(() -> {
+            System.out.print("\nMinecraft username: ");
+            String username = reader.readLine().trim();
+
+            HttpResponse<String> str = Unirest.get(UUID_URL + username).asString();
+
+            if (!str.isSuccess() || str.getStatus() != 200) {
+                System.out.println("Could not get UUID for user '" + username + "'. Status: " + str.getStatus());
+                throw new IOException("Cannot find username");
+            }
+
+            UuidNameResponse res = g.fromJson(str.getBody(), UuidNameResponse.class);
+            System.out.println("Found user '" + res.name + "' with UUID '" + res.id + "'");
+
+            return res.id;
+        });
     }
 
     /**
      * Get the contents of the Minecraft launcher_profiles.json from the given installation path.
      * @return the contents of the file
      */
-    private String getFile() {
+    private String getFile() throws IOException {
         String path = Game.getGamePath();
 
         // handle common %APPDATA% env variable for Windows
@@ -43,14 +129,17 @@ public class ClientAuthenticator {
 
         Path p = Paths.get(path, "launcher_profiles.json");
 
-        try {
-            return String.join("\n", Files.readAllLines(p));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Cannot read or find '" + p.toString() + "'!\nUse launch option: \"-m /path/to/.minecraft/\" to indicate the location of your Minecraft installation.");
-            System.exit(0);
+        System.out.println("Reading profile information from " + p.toString());
+
+        return String.join("\n", Files.readAllLines(p));
+    }
+
+    private AuthDetails getAuthDetails() {
+        if (profiles != null) {
+            return profiles.getAuthDetails();
+        } else {
+            return manualDetails;
         }
-        return null;
     }
 
     /**
@@ -60,9 +149,10 @@ public class ClientAuthenticator {
      * @param hash hash based on the server information.
      */
     public void makeRequest(String hash) throws UnirestException {
-        AuthDetails details = profiles.getAuthDetails();
+        AuthDetails details = getAuthDetails();
 
         Map<String, String> body = new HashMap<>();
+
         body.put("accessToken", details.getAccessToken());
         body.put("selectedProfile", details.getUuid());
         body.put("serverId", hash);
@@ -79,4 +169,13 @@ public class ClientAuthenticator {
             System.out.println("Successfully authenticated user with Mojang session server.");
         }
     }
+}
+
+interface Prompt {
+    String run() throws IOException;
+}
+
+class UuidNameResponse {
+    String id;
+    String name;
 }
