9379c5c10f0032449efe72d67110fea36df2ce47
Mirco Kroon <23699979+mircokroon@users.noreply.github.com>
2021-02-21 00:13:21 +0100

    Fix incorrect tile entity names
    #80

--- src/main/java/game/data/chunk/ChunkEntities.java
+++ src/main/java/game/data/chunk/ChunkEntities.java
@@ -1,16 +1,18 @@
 package game.data.chunk;
 
 import game.data.WorldManager;
+import game.data.chunk.palette.BlockState;
 import game.data.container.InventoryWindow;
 import game.data.coordinates.Coordinate3D;
 import game.data.coordinates.CoordinateDim2D;
 import game.data.coordinates.CoordinateDim3D;
 import game.data.dimension.Dimension;
-import game.data.entity.Entity;
 import se.llbit.nbt.*;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Manage entities and tile entities for chunks.
@@ -56,11 +58,29 @@ public abstract class ChunkEntities {
         if (!entity.get("id").isError()) {
             String id = entity.get("id").stringValue();
 
+            if (id.split(":").length < 2) {
+                id = "minecraft:" + id.toLowerCase();
+            }
+
             // invalid identifier - some servers will send these and it makes Minecraft angry when we load the world
             if (!id.matches("^[a-z0-9/._-]*$")) {
                 entity.add("id", new StringTag(id.toLowerCase()));
             }
+
+
+            // some servers send slightly incorrect tile entity IDs (e.g. chest for trapped_chests), we can fix those
+            // to ensure that the chests still works
+            BlockState bs = getBlockStateAt(location.withinChunk());
+            if (bs != null) {
+                String blockStateName = bs.getName();
+
+                if (!blockStateName.equals(id) && blockStateName.contains("chest")) {
+                    entity.add("id", new StringTag(bs.getName()));
+                }
+            }
         }
+
+
 
         // get offset location
         Coordinate3D offset = location.offsetGlobal();
@@ -91,4 +111,5 @@ public abstract class ChunkEntities {
 
     public abstract Dimension getDimension();
     public abstract CoordinateDim2D getLocation();
+    public abstract BlockState getBlockStateAt(Coordinate3D location);
 }
